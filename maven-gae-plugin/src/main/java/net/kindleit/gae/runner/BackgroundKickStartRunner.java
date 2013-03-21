1e5ba1395fb3a64a9b01543b5d093d655be7a3ac
Nathan Toone <nathan@toonetown.com>
2013-03-21 10:32:43 -0600

    Problem: gae:start not forking
    Since the string that is printed out by the local appengine server has changed, we need to watch for it instead. We change the string to "is running" to be a bit more robust in the future as well.

--- maven-gae-plugin/src/main/java/net/kindleit/gae/runner/BackgroundKickStartRunner.java
+++ maven-gae-plugin/src/main/java/net/kindleit/gae/runner/BackgroundKickStartRunner.java
@@ -131,14 +131,14 @@ final class BackgroundKickStartRunner extends KickStartRunner {
 
   private synchronized void consumeOutputLine(final String line) {
     System.out.println(line);
-    if (line.contains("The server is running")) {
+    if (line.contains("is running")) {
       notify();
     }
   }
 
   private synchronized void consumeErrorLine(final String line) {
     System.err.println(line);
-    if (line.contains("The server is running")) {
+    if (line.contains("is running")) {
       notify();
     }
   }
